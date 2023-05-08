cf3605786a846f986737959972aba86a95dc7e29
Tiago Silva <57015073+surumkata@users.noreply.github.com>
2023-05-08 21:10:12 +0100

    TPC7 - modelo + tratamento 'html'

--- TPC7/cria_modelo.py
+++ TPC7/cria_modelo.py
@@ -3,7 +3,7 @@ import argparse
 from glob import glob
 from gensim.models import Word2Vec
 from gensim.utils import tokenize
-
+from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser(
     prog="Criar Modelo",
@@ -14,10 +14,15 @@ parser.add_argument('dir')
 parser.add_argument('-e','--epochs',default=25)
 parser.add_argument('-d','--dimension',default=300)
 parser.add_argument('-o','--output',default='modelo')
+parser.add_argument('-html',default=False,action='store_true')
 
 args = parser.parse_args()
 dir = args.dir
-files = glob(f'{dir}/*.txt')
+html = args.html
+if(not html): 
+    files = glob(f'{dir}/*.txt')
+else: 
+    files = glob(f'{dir}/*.html',recursive=True)
 
 epochs = args.epochs
 dim = args.dimension
@@ -25,10 +30,22 @@ out = args.output
 
 
 cont = []
-for file in files:
-    f = open(file,'r',encoding="utf8")
-    for line in f:
-        cont.append(list(tokenize(line,lower=True)))
+if(not html):
+    for file in files:
+        f = open(file,'r',encoding="utf8")
+        for line in f:
+            cont.append(list(tokenize(line,lower=True)))
+        f.close()
+else:
+    for file in files:
+        f = open(file,'r',encoding="utf8")
+        html = f.read()
+        soup = BeautifulSoup(html,features='lxml')
+        for elem in soup.find_all('text'):
+            text = elem.get_text()
+            for line in text.splitlines():
+                cont.append(list(tokenize(line,lowercase=True)))
+        f.close()
 
 model = Word2Vec(cont,epochs=epochs, vector_size=dim)
 
